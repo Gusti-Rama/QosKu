edaeb14a98796baf6d1b75e204ca5b365df85e87
Gusti-Rama <gustirama309@gmail.com>
2025-06-08 22:10:22 +0700

    sign in fix & dashboard

---
+++ dashboardKamar.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QosKu</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="icon" href="./img/QosKuIMG.png" type="image/png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+</head>
+
+<body class="bg-light">
+    <div class="d-flex min-vh-100 ms-4 me-4">
+        <nav class="bg-transparent p-3 d-flex flex-column" style="width: 250px;">
+            <a class="navbar-brand fw-bold fs-3 pt-3 border-bottom" href="#" style="color: #2D3748;">
+                <img src="./img/QosKuIMG.png" class="mb-1" alt="Logo" height="80">QosKu
+            </a>
+            <div class="flex-grow-1 d-flex flex-column mt-3">
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2">
+                        <div class="bg-white rounded-4 shadow-sm py-2">
+                            <a href="#" class="nav-link text-dark fw-bold"><i class="bi bi-house-door-fill text-dark"></i> Kamar Anda</a>
+                        </div>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a href="#" class="nav-link text-secondary"><i class="bi bi-credit-card"></i> Pesan Kamar</a>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a href="#" class="nav-link text-secondary"><i class="bi bi-person-fill"></i> Profil</a>
+                    </li>
+                    <li class="nav-item mb-2">
+                        <a href="#" class="nav-link text-secondary"><i class="bi bi-box-arrow-right me-2"></i>Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="flex-grow-1">
+            <div class="d-flex justify-content-between mt-4 px-4 pt-3 bg-transparent">
+                <div>
+                    <p class="mb-0 fs-6 text-secondary">Pages <b>/ Kamar Anda</b></p>
+                    <p class="fs-5 fw-bold">Kamar Anda</p>
+                </div>
+                <div class="d-flex align-items-start gap-3">
+                    <div class="input-group input-group-sm">
+                        <span class="input-group-text bg-white rounded-4 border-end-0 rounded-end-0">
+                            <i class="bi bi-search"></i>
+                        </span>
+                        <input type="text" class="form-control border-start-0 rounded-4 rounded-start-0" placeholder="Pencarian">
+                    </div>
+                    <div class="d-flex align-items-center gap-1">
+                        <i class="bi bi-person-fill fs-5"></i>
+                        <span class="fs-6">Profil</span>
+                    </div> <i class="bi bi-gear-fill fs-5"></i>
+                    <i class="bi bi-bell-fill fs-5"></i>
+                </div>
+            </div>
+
+            <div class="container-fluid pt-4 pb-3">
+                <div class="row mb-4">
+                    <div class="col-md-4">
+                        <div class="card shadow-sm border-0">
+                            <div class="card-body">
+                                <p class="card-title fs-5 fw-bold text-secondary">Tagihan Anda</p>
+                                <p class="card-text fs-3 fw-bold">Rp660,000 <i class="bi bi-wallet2 text-end"></i></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card shadow-sm border-0">
+                            <div class="card-body">
+                                <p class="card-title fs-5 fw-bold text-secondary">Harga Kamar</p>
+                                <p class="card-text fs-3 fw-bold">Rp600,000 / Bulan</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card shadow-sm border-0">
+                            <div class="card-body">
+                                <p class="card-title fs-5 fw-bold text-secondary">Tambahan Listrik</p>
+                                <p class="card-text fs-3 fw-bold">Rp60,000 / Bulan</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="card shadow-sm border-0 rounded-4 text-white"
+                            style="background-image: url('./img/backgroundKamar.png'); background-size: cover; background-position: center; min-height: 300px;">
+                            <div class="card-body rounded-4 p-4">
+                                <h5 class="card-title fw-bold">Kamar No. 1.15</h5>
+                            </div>
+                            <div class="d-flex justify-content-center my-4">
+                                <button class="btn btn-light text-dark px-4 rounded-3 fw-bold w-50">Lihat Detail</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- signIn.php
+++ signIn.php
@@ -8,31 +8,27 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
     <link rel="icon" href="./img/QosKuIMG.png" type="image/png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 </head>
-
 <body>
-    <div class="position-fixed top-0 start-50 translate-middle-x mt-4 mx-4 pe-5" style="z-index: 1030; width: 70%;">
+    <div class="position-fixed top-0 start-50 translate-middle-x mt-4 mx-4" style="z-index: 10; width: 70%;">
         <nav class="navbar navbar-expand-lg navbar-light bg-light shadow rounded-3 pe-5">
-            <!-- Logo -->
             <a class="navbar-brand fw-bold" href="#" style="color: #2D3748;">
-                <img src="./img/QosKuIMG.png" alt="Logo" height="60">
-                QosKu
+                <img src="./img/QosKuIMG.png" class="mb-1" alt="Logo" height="60">QosKu
             </a>
-
-            <!-- Menu links -->
             <div class="collapse navbar-collapse justify-content-center me-5 pe-5" id="navbarMenu">
                 <ul class="navbar-nav mb-2 mb-lg-0">
                     <li class="nav-item px-2">
-                        <a class="nav-link active fw-medium" href="#">DASHBOARD</a>
+                        <a class="nav-link fw-medium" href="dashboardKamar.php"><i class="bi bi-box-fill"></i> DASHBOARD</a>
                     </li>
                     <li class="nav-item px-2">
-                        <a class="nav-link fw-medium" href="#">PROFILE</a>
+                        <a class="nav-link fw-medium" href="#"><i class="bi bi-person-fill"></i> PROFILE</a>
                     </li>
                     <li class="nav-item px-2">
-                        <a class="nav-link fw-medium" href="#">SIGN UP</a>
+                        <a class="nav-link fw-medium" href="#"><i class="bi bi-person-fill-add"></i> SIGN UP</a>
                     </li>
                     <li class="nav-item px-2">
-                        <a class="nav-link fw-medium" href="#">SIGN IN</a>
+                        <a class="nav-link active fw-medium" href="#"><i class="bi bi-key-fill"></i> SIGN IN</a>
                     </li>
                 </ul>
             </div>
@@ -42,9 +38,9 @@
     <div class="container-fluid full-height">
         <div class="row h-100">
             <div class="col-md-6 d-flex justify-content-center align-items-center bg-white">
-                <form class="w-50">
-                    <h2 class="text-center mb-4 fw-bold">Selamat Datang</h2>
-                    <p class="fw-bold">Masukkan username dan password untuk login</p>
+                <form class="w-50 mt-5">
+                    <h2 class="mb-3 mt-5 fw-bold">Selamat Datang</h2>
+                    <p class="mb-5 fw-bold text-secondary">Masukkan username dan password untuk login</p>
                     <div class="mb-3">
                         <label for="username" class="form-label">Username</label>
                         <input type="text" class="form-control" id="username" placeholder="Username anda">
@@ -53,12 +49,16 @@
                         <label for="password" class="form-label">Password</label>
                         <input type="password" class="form-control" id="password" placeholder="Password anda">
                     </div>
-                    <button type="submit" class="btn btn-dark w-100">Login</button>
-                    <p class="mt-2">Belum memiliki akun? <a href="#">Daftar</a></p>
+                    <div class="form-check form-switch mb-3">
+                        <input class="form-check-input" type="checkbox" id="ingat">
+                        <label class="form-check-label" for="ingat">Ingat Saya</label>
+                    </div>  
+                    <button type="submit" class="btn btn-dark w-100">Masuk</button>
+                    <p class="mt-2 text-center text-secondary">Belum memiliki akun? <a href="#" class="text-dark">Daftar</a></p>
                 </form>
             </div>
             <div class="col-md-6 d-flex justify-content-center align-items-center bg-white position-relative" style="background-image: url('./img/background.png'); background-size: cover; background-position: center;">
-                <img src="./img/QosKuNoBG.png" class="img-fluid" alt="Logo" >
+                <img src="./img/QosKuNoBG.png" class="img-fluid" alt="Logo">
             </div>
 
         </div>
